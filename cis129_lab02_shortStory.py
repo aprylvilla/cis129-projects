38759ffe0bf8f9dadf121372eaa3e7a7bfbe8948
aprylvilla <avillatautimez@mail.pima.edu>
2025-02-05 15:31:39 -0700

    Update cis129_lab02_shortStory.py
    different print () statements
    comments

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,3 +1,7 @@
+# Author: Apryl Villa
+# My first Python lab in  CIS129 course
+# Prints a simple short story 
+
 print ("Bacon, bacon, bacon.")
 print ("orange cats are the best.")
 print ("food.")
